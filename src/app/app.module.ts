71d72f805c8d1e856fc5f4827e6875311d0aa818
Stephen Ciesla <s.ciesla@gmail.com>
2017-04-21 21:41:23 -0400

    Added kendo button and theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ButtonsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
